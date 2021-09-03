62b18061549ffe42caa0708aa1ec9182477d648f
0x6666 <87167655+0X6666666@users.noreply.github.com>
2021-09-03 01:07:51 +0100

    Create rust-offsets.h

---
+++ rust-offsets.h
@@ -0,0 +1,8 @@
+    
+    
+    
+    constexpr uint64_t gom_offset = 0x17C1F18;
+    constexpr uint64_t bn_offset = 0x329B028; // BaseNetworkable_c* 48 8b 05 ? ? ? ? 48 8b 88 ? ? ? ? 48 8b 09 48 85 c9 74 ? 45 33 c0 8b
+    constexpr uint64_t fov_offset = 0x3298EE0; // ConVar_Graphics_c*
+    constexpr uint64_t global_offset = 0x329B6D8; // ConVar_Global_c*
+    constexpr uint64_t admin_offset = 0x32950D8; // ConVar_Admin_c*
